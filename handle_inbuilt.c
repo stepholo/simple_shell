6449b3612e3e5d274c9559469f71c3fc40c659f4
stepholo <oloobrian89@gmail.com>
2023-05-19 05:23:39 -0700

    Handle inbuilt command

--- handle_inbuilt.c
+++ handle_inbuilt.c
@@ -1,4 +1,4 @@
-#inlcude "main.h"
+#include "main.h"
 
 /**
 * inbuilt_command - Function that handles shell built-in commands
@@ -11,18 +11,11 @@
 int inbuilt_command(char *command, char **args, char **envp, char **av)
 {
 	(void)av;
+	(void)envp;
+	(void)args;
 
 	if (command == NULL)
 		return (-1);
-
-	if (_strcmp(command, "exit") == 0)
-		return (exit_builtin(args));
-
-	if (_strcmp(command, "env") == 0)
-		return (env_builtin(envp));
-
-	if (_strcmp(command, "cd") == 0)
-		return (cd(args));
 
 	/* Add any additional built-in commands here...  */
 
