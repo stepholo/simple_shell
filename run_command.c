dcadcbed14555b7bd96d388731f7b25a1d87ab6e
stepholo <oloobrian89@gmail.com>
2023-05-19 05:20:30 -0700

    Run command

--- run_command.c
+++ run_command.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * execute_command - executes the command given
- * @path: path of the command
- * @args: Command arguments
- * @envp: environment
- * Return: status
- */
+* execute_command - executes the command given
+* @path: path of the command
+* @args: Command arguments
+* @envp: environment
+* Return: status
+*/
 int execute_command(char *path, char **args, char **envp)
 {
 	pid_t pid;
@@ -22,10 +22,12 @@ int execute_command(char *path, char **args, char **envp)
 	{
 		execve(path, args, envp);
 
-		if (_strcmp(args[0], "exit") == 0)
-			exit_builtin(args);
+		if (_strcmp(args[0], "cd") == 0)
+			cd(args);
+
 		else
 			perror("execve() failed");
+
 		exit(1);
 	}
 	else
@@ -36,22 +38,24 @@ int execute_command(char *path, char **args, char **envp)
 }
 
 /**
- * run_command - Run the command given
- * @command: Input from the user
- * @args: array of arguments
- * @envp: environment variable
- * @av: The program name and arguments
- * @line_number: Keep track of the command number
- * Return: 0 on success, -1 on error
- */
+* run_command - Run the command given
+* @command: Input from the user
+* @args: array of arguments
+* @envp: environment variable
+* @av: The program name and arguments
+* @line_number: Keep track of the command number
+* Return: 0 on success, -1 on error
+*/
 int run_command(char *command, char **args, char **envp,
 		char **av, int line_number)
 {
 	char *path = NULL;
 	int status;
 
-	if (inbuilt_command(command, args, envp, av) == 0)
+	/*
+	 *if (inbuilt_command(command, args, envp, av) == 0)
 		return (0);
+	*/
 
 	if (command != NULL && access(command, X_OK) == 0)
 		path = _strdup(command);
