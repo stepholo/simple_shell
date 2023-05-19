e109f21903c99f7587d3f7c4c396fe5f173e9a1a
stepholo <oloobrian89@gmail.com>
2023-05-19 05:22:59 -0700

    Handle error messages

--- error_message.c
+++ error_message.c
@@ -27,7 +27,7 @@ void _sprintf(char *str, int num)
 	for (i = 0; i < len / 2; i++)
 	{
 		temp = str[i];
-		str[i] = str[len - i = 1];
+		str[i] = str[len - i - 1];
 		str[len - i - 1] = temp;
 	}
 
